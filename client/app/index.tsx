eadeff8acb91c36934edb9dfc90a5ddc444f21d7
zhangyue325 <94215643+zhangyue325@users.noreply.github.com>
2025-04-07 17:33:20 +0800

    Update index.tsx

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -1,6 +1,21 @@
-// app/index.tsx
+import { useEffect, useState } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
 import { Redirect } from 'expo-router';
+import { auth } from '../firebase';
 
 export default function Index() {
-  return <Redirect href='/(tabs)' />;
+  const [userChecked, setUserChecked] = useState(false);
+  const [user, setUser] = useState<any>(null);
+
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, (u) => {
+      setUser(u);
+      setUserChecked(true);
+    });
+    return unsub;
+  }, []);
+
+  if (!userChecked) return null;
+
+  return <Redirect href={user ? '/(tabs)' : '/welcome'} />;
 }
