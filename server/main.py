6cc91aa4c5371bd10b7c42539358f32915da0afb
zhangyue325 <zhangyue325@gmail.com>
2025-04-07 22:46:15 +0800

    update

--- server/main.py
+++ server/main.py
@@ -3,8 +3,8 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from routes.chat import router as chat_router
 from routes.discover import router as discover_router
-from routes.history import router as history_router
 from routes.characters import router as characters_router
+from routes.history import router as history_router
 
 # server % uvicorn main:app --host 0.0.0.0 --port 8000
 
@@ -20,5 +20,5 @@ app.add_middleware(
 
 app.include_router(chat_router, prefix="/chat", tags=["Chat"])
 app.include_router(discover_router, prefix="/discover", tags=["Discover"])
-app.include_router(history_router, prefix="/history")
 app.include_router(characters_router, prefix="/characters", tags=["Characters"])
+app.include_router(history_router, prefix="/history", tags=["Discover"])
